c6c8204bb109346e7b2402bfab4cf7b0da8a0c43
Vitor Oliveira <vitor8684oliveira@gmail.com>
2026-06-05 19:09:44 -0300

    corrigindo requirements e main.py. Adiciondo dir uploads

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI;
 from fastapi.staticfiles import StaticFiles;
 from app.routes import files;
 
+import os;
 
+os.makedirs("uploads", exist_ok=True);
 
 app = FastAPI();
 
